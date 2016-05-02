ab70d3f3136d43a7c6fe7b1f16563eb7ffef8126
lily <lilyl2@cisco.com>
2016-05-02 13:45:47 -0400

    performance enhancements; cleaned up pygen_tests.py

--- ydkgen/printer/python/python_rst_printer.py
+++ ydkgen/printer/python/python_rst_printer.py
@@ -49,32 +49,36 @@ class PythonRstPrinter(object):
         lines.append('=' * len(title))
         lines.append('')
         lines.append('.. toctree::')
-        self.ctx.writelns(lines)
 
         self.ctx.lvl_inc()
-        lines = []
-        lines.append(':maxdepth: 1\n')
+        lines.append('%s:maxdepth: 1\n' % self.ctx.get_indent())
 
         for package in packages:
             line = '%s <%s>' % (package.name, get_rst_file_name(package))
-            lines.append(line)
+            lines.append('%s%s' % (self.ctx.get_indent(), line))
 
-        self.ctx.writelns(lines)
         self.ctx.lvl_dec()
+        self.ctx.writelns(lines)
 
-    def _write_toctree(self, named_element):
-        self.ctx.writeln('.. toctree::')
+    def _write_toctree(self, named_element, lines):
+        # lines = []
+        lines.append('%s.. toctree::' % self.ctx.get_indent())
+        
         self.ctx.lvl_inc()
-        lines = []
-        lines.append(':maxdepth: 1\n')
+        lines.append('%s:maxdepth: 1\n' % self.ctx.get_indent())
+
         owned_elements = named_element.owned_elements
         owned_elements.reverse()
-        for c in owned_elements:
-            if isinstance(c, Class) or isinstance(c, Enum):
-                lines.append('%s <%s>' % (c.name, get_rst_file_name(c)))
-        lines.append('')
-        self.ctx.writelns(lines)
+        
+        for elem in owned_elements:
+            if isinstance(elem, Class) or isinstance(elem, Enum):
+                lines.append('%s%s <%s>' % (self.ctx.get_indent(), elem.name, get_rst_file_name(elem)))
+            # if isinstance(elem, Enum):
+                # from pdb import set_trace; set_trace()
+                # print 'hi'
+        lines.append('\n')
         self.ctx.lvl_dec()
+        # self.ctx.writelns(lines)
 
     def _get_class_hierarchy(self, clazz):
         parent_list = []
@@ -104,13 +108,13 @@ class PythonRstPrinter(object):
         lines.append(clazz.name)
         lines.append('=' * len(clazz.name))
         lines.append('\n')
-        self.ctx.writelns(lines)
+        # self.ctx.writelns(lines)
 
         # TOC Tree
-        self._write_toctree(clazz)
+        self._write_toctree(clazz, lines)
 
-        lines = []
-        lines.append('')
+        # lines = []
+        lines.append('\n')
         lines.append('.. py:currentmodule:: %s' %
                          (clazz.get_py_mod_name()))
         lines.append('\n')
@@ -119,49 +123,50 @@ class PythonRstPrinter(object):
         lines.append('.. py:class:: %s' % (clazz.qn()))
         lines.append('\n')
 
-        self.ctx.writelns(lines)
+        # self.ctx.writelns(lines)
         self.ctx.lvl_inc()
 
         # Bases
-        lines = []
-        bases = [':class:`object`']
+        # lines = []
+        bases = ['%s:class:`object`' % self.ctx.get_indent()]
         if clazz.extends:
             for item in clazz.extends:
-                bases.append(':class:`%s`' % (item.name))
-        lines.append('Bases: %s' % (', '.join(bases)))
+                bases.append('%s:class:`%s`' % (self.ctx.get_indent(), item.name))
+        lines.append('%sBases: %s' % (self.ctx.get_indent(), ', '.join(bases)))
         lines.append('\n')
 
         # Class Hierarchy
         if not clazz.is_identity() and not clazz.is_grouping():
             clazz_hierarchy = self._get_class_hierarchy(clazz)
             if clazz_hierarchy is not None:
-                lines.append(clazz_hierarchy)
+                lines.append('%s%s' % (self.ctx.get_indent(), clazz_hierarchy))
                 lines.append('\n')
 
         # Presence Container
         lines.append('\n')
         if clazz.stmt.search_one('presence') is not None:
             line = """This class is a :ref:`presence class<presence-class>`"""
-            lines.append(line)
+            lines.append('%s%s' % (self.ctx.get_indent(), line))
             lines.append('\n')
 
         # Doc String
         if len(class_docstring) > 0:
             for line in class_docstring.split('\n'):
                 if line.strip() != '':
-                    lines.append(line)
+                    lines.append('%s%s' % (self.ctx.get_indent(), line))
                     lines.append('\n')
 
         if not clazz.is_identity() and not clazz.is_grouping():
             # Config Method
-            lines.append('.. method:: is_config()\n')
-            self.ctx.writelns(lines)
+            lines.append('%s.. method:: is_config()\n' % (self.ctx.get_indent()))
             self.ctx.lvl_inc()
-            self.ctx.writeln("Returns True if this instance \
-                represents config data else returns False")
+            lines.append("%sReturns True if this instance \
+                represents config data else returns False" % (self.ctx.get_indent()))
             self.ctx.lvl_dec()
-            self.ctx.bline()
+            lines.append('\n')
+
         self.ctx.lvl_dec()
+        self.ctx.writelns(lines)
 
     def _print_package_rst(self, package):
         # Header
@@ -172,10 +177,9 @@ class PythonRstPrinter(object):
         lines.append(line)
         lines.append('=' * len(line))
         lines.append('\n')
-        self.ctx.writelns(lines)
-        self._write_toctree(package)
+        
+        self._write_toctree(package, lines)
 
-        lines = []
         lines.append('\n')
         lines.append('.. py:module:: %s.%s' %
                          (package.get_py_mod_name(), package.name))
@@ -189,6 +193,7 @@ class PythonRstPrinter(object):
         self.ctx.writelns(lines)
 
     def _print_enum_rst(self, enumz):
+        # from pdb import set_trace; set_trace()
         lines = []
         # Title
         line = enumz.name
@@ -203,13 +208,11 @@ class PythonRstPrinter(object):
         lines.append('.. py:class:: %s' % (enumz.qn()))
         lines.append('\n')
 
-        self.ctx.writelns(lines)
         self.ctx.lvl_inc()
 
         # Bases
-        lines = []
-        bases = [':class:`enum.Enum`']
-        lines.append('Bases: %s' % (', '.join(bases)))
+        bases = ['%s:class:`enum.Enum`' % (self.ctx.get_indent())]
+        lines.append('%sBases: %s' % (self.ctx.get_indent(), ', '.join(bases)))
         lines.append('\n')
 
         enumz_docstring = get_enum_class_docstring(enumz)
@@ -217,8 +220,8 @@ class PythonRstPrinter(object):
         if len(enumz_docstring):
             for line in enumz_docstring.split('\n'):
                 if line.strip() != '':
-                    lines.append(line)
+                    lines.append('%s%s' % (self.ctx.get_indent(), line))
                     lines.append('\n')
 
-        self.ctx.writelns(lines)
         self.ctx.lvl_dec()
+        self.ctx.writelns(lines)
