eae1c1fd1a4e02f7d722f46830724322e9411929
Abhi Keshav <abhirame@cisco.com>
2018-04-03 08:59:16 -0700

    Reduced generated py code size

--- sdk/python/core/ydk/types/py_types.py
+++ sdk/python/core/ydk/types/py_types.py
@@ -124,10 +124,8 @@ class Entity(_Entity):
         self._local_refs = {}
         self._children_name_map = OrderedDict()
         self._children_yang_names = set()
-        self._child_container_classes = OrderedDict()
-        self._child_list_classes = OrderedDict()
+        self._child_classes = OrderedDict()
         self._leafs = OrderedDict()
-        self._child_list_container_order = []
         self._segment_path = lambda : ''
         self._absolute_path = lambda : ''
 
@@ -168,7 +166,8 @@ class Entity(_Entity):
 
     def get_order_of_children(self):
         order = []
-        for name in self._child_list_container_order:
+        for yang_name in self._child_classes:
+            name = self._child_classes[yang_name][0]
             value = self.__dict__[name]
             if isinstance(value, YList):
                 for v in value:
@@ -184,26 +183,20 @@ class Entity(_Entity):
             return child
 
         found = False
-        is_container = True
-        if child_yang_name in self._child_container_classes:
+        if child_yang_name in self._child_classes:
             found = True
-        elif child_yang_name in self._child_list_classes:
-            found = True
-            is_container = False
         if found:
-            if is_container:
-                attr, clazz = self._child_container_classes[child_yang_name]
-            else:
-                attr, clazz = self._child_list_classes[child_yang_name]
+            attr, clazz = self._child_classes[child_yang_name]
+            is_list = isinstance(getattr(self, attr), YList)
             child = clazz()
             child.parent = self
-            if is_container:
-                self._children_name_map[attr] = child_yang_name
-                setattr(self, attr, child)
-            else:
+            if is_list:
                 local_reference_key = "ydk::seg::%s" % segment_path
                 self._local_refs[local_reference_key] = child
                 getattr(self, attr).append(child)
+            else:
+                self._children_name_map[attr] = child_yang_name
+                setattr(self, attr, child)
 
             return child
 
@@ -275,10 +268,7 @@ class Entity(_Entity):
             if name == leaf.name:
                 return True
 
-        if name in self._child_list_classes:
-            return True
-
-        if name in self._child_container_classes:
+        if name in self._child_classes:
             return True
 
         return False
@@ -352,7 +342,7 @@ class Entity(_Entity):
                 if hasattr(value, "parent") and name != "parent":
                     if hasattr(value, "is_presence_container") and value.is_presence_container:
                         value.parent = self
-                    elif value.parent is None and value.yang_name in self._children_yang_names:
+                    elif value.parent is None and value.yang_name in self._child_classes:
                         value.parent = self
                 super(Entity, self).__setattr__(name, value)
 
